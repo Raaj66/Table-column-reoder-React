f0e6ca4d1d855794eda274224e3be3c3a92c5a5a
Raaj66 <thyagaraju@openturf.in>
2021-03-24 11:35:14 +0530

    UPLOAD

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { Grid, GridColumn as Column } from '@progress/kendo-react-grid';
+import '@progress/kendo-theme-default/dist/all.css';
+import products from './products.json';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid
+    // style={{ height: '400px' }}
+    data={[ ...products ]}
+    reorderable
+>
+<Column field="ProductID" title="ID" width="40px" />
+                <Column field="ProductName" title="Name" width="250px" />
+                <Column field="Category.CategoryName" title="CategoryName" />
+                <Column field="UnitPrice" title="Price" />
+                <Column field="UnitsInStock" title="In stock" />
+                <Column
+                    field="Discontinued"
+                    cell={props => (
+                        <td>
+                            <input disabled type="checkbox" checked={props.dataItem[props.field]} />
+                        </td>
+                    )}
+                />
+</Grid>
   );
 }
 
